3dba0eb02bff3b889b57d5a95c34cc666d6d470f
Steve Babb <steve.babb@outlook.com>
2021-08-28 16:21:34 -0500

    Update

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,39 +2,23 @@
 
 console.log("app.js is running");
 
-// JSX - JavaScript XML
-
-var app = {
-  title: "Indecision App",
-  subTitle: "Since you can't decide, let a machine decide!",
-  options: []
+var content = {
+  title: "Visibility Toggle",
+  details: "Hi, I am the details"
 };
 
-var onFormSubmit = function onFormSubmit(e) {
-  e.preventDefault();
-  var option = e.target.elements.option.value;
-  if (option) {
-    app.options.push(option);
-    e.target.elements.option.value = "";
+var changeBtn = "Show Details";
 
-    console.log(app.options);
+var showDetails = function showDetails(e) {
+  e.preventDefault();
+  changeBtn = e.target.innerText;
+  if (changeBtn === "Show Details") {
+    changeBtn = "Hide Details";
+  } else {
+    changeBtn = "Show Details";
   }
   render();
 };
-
-var removeAllOptions = function removeAllOptions() {
-  app.options = [];
-  render();
-};
-
-var makeDecision = function makeDecision() {
-  var randomNum = Math.floor(Math.random() * app.options.length);
-  var option = app.options[randomNum];
-  alert(option);
-  render();
-};
-
-console.log(app.options);
 
 var render = function render() {
   var template = React.createElement(
@@ -43,60 +27,22 @@ var render = function render() {
     React.createElement(
       "h1",
       null,
-      app.title
-    ),
-    app.subTitle && React.createElement(
-      "h3",
-      null,
-      app.subTitle
-    ),
-    app.options && app.options.length > 0 ? React.createElement(
-      "h4",
-      null,
-      "Here are your options:",
-      React.createElement(
-        "ol",
-        null,
-        app.options.map(function (item, index) {
-          return React.createElement(
-            "li",
-            { key: index },
-            item
-          );
-        })
-      )
-    ) : "No options",
-    React.createElement(
-      "form",
-      { onSubmit: onFormSubmit },
-      React.createElement("input", { type: "text", name: "option" }),
-      React.createElement(
-        "button",
-        null,
-        "Add Option"
-      )
+      content.title
     ),
     React.createElement(
-      "div",
-      null,
-      React.createElement(
-        "button",
-        { disabled: app.options.length === 0, onClick: removeAllOptions },
-        "Remove All Options"
-      )
+      "button",
+      { name: "option", onClick: showDetails },
+      changeBtn
     ),
-    React.createElement(
-      "div",
+    changeBtn === "Hide Details" && React.createElement(
+      "p",
       null,
-      React.createElement(
-        "button",
-        { disabled: app.options.length === 0, onClick: makeDecision },
-        "What should I do?"
-      )
+      content.details
     )
   );
   ReactDOM.render(template, appRoot);
 };
 
 var appRoot = document.getElementById("app");
+
 render();
